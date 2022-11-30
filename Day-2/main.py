cd5468c9ae5589a709615763b7fff2bff283dc84
Vaibhav Tyagi <VAIBHAVTYAGI438@GMAIL.COM>
2022-11-30 21:41:42 +0530

    day-2 completed

---
+++ Day-2/main.py
@@ -0,0 +1,6 @@
+import pandas  # This is an example of external module
+import hashlib  # This is an example of built in module
+
+print("Hi")
+
+# Dont worry about how to use these modules just yet!
